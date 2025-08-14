96fc97055f6b068bc157408294288db541c2d09c
bethanyhipple-dbtlabs <108838013+bethanyhipple-dbtlabs@users.noreply.github.com>
2025-08-14 10:25:07 -0700

    Delete analysis/solutions directory

--- analysis/solutions/custom_tests/check_decimal_data_type.sql
+++
@@ -1,9 +0,0 @@
-{% test check_decimal_data_type(model, column_name) %}
-    
-select 
-    {{ column_name }},
-    typeof({{ column_name }}) as {{ column_name }}_data_type
-from {{ model}}
-where typeof({{ column_name }}) <> 'DECIMAL'
-
-{% endtest %}
