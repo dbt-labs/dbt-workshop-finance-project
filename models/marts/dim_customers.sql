13c9cefa4fb4f378a4431f1271f103b9ffff15ef
bethanyhipple-dbtlabs <108838013+bethanyhipple-dbtlabs@users.noreply.github.com>
2026-04-04 15:18:26 -0700

    Update customer_orders to use customer_key

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -6,24 +6,23 @@ orders as (
 ),
 customer_orders as (
     select
-        customer_id,
+        customer_key,
         min (order_date) as first_order_date,
         max (order_date) as most_recent_order_date,
         count(order_id) as number_of_orders,
-        sum(amount) as lifetime_value
+        sum(gross_item_sales_amount) as lifetime_value
     from orders
     group by 1
 ),
  final as (
     select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
+        customers.customer_key,
+        customers.name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce (customer_orders.number_of_orders, 0) as number_of_orders,
         customer_orders.lifetime_value
     from customers
-    left join customer_orders using (customer_id)
+    left join customer_orders using (customer_key)
 )
 select * from final
