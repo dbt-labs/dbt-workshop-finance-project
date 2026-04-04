10f77f2169dcdf19f7cd4c4ce7a4223fb89527d6
bethanyhipple-dbtlabs <108838013+bethanyhipple-dbtlabs@users.noreply.github.com>
2026-04-04 15:19:37 -0700

    Change order_id to order_key in count aggregation

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -9,7 +9,7 @@ customer_orders as (
         customer_key,
         min (order_date) as first_order_date,
         max (order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders,
+        count(order_key) as number_of_orders,
         sum(gross_item_sales_amount) as lifetime_value
     from orders
     group by 1
